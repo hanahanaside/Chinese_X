85409b5c7bfbc933c05be17e6ba707f11f9a00bf
hanahanaside <flower@flower-no-MacBook-Air.local>
2014-12-21 21:16:51 +0900

    ボス作成完了

---
+++ Assets/Scripts/Barrel/Barrel.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Barrel : MonoBehaviour {
+
+	public float moveForce;
+	public float maxSpeed;
+	private Transform mPlayerTransform;
+	private float mMoveSpeed;
+
+	void Start () {
+		mPlayerTransform = Player.instance.transform;
+		if(transform.position.x > mPlayerTransform.position.x){
+			moveForce = -moveForce;
+		}
+	}
+
+	void Update(){
+		rigidbody2D.AddForce (Vector2.right *  moveForce);
+		// If the player's horizontal velocity is greater than the maxSpeed...
+		if (Mathf.Abs (rigidbody2D.velocity.x) > maxSpeed) {
+			// ... set the player's velocity to the maxSpeed in the x axis.
+			rigidbody2D.velocity = new Vector2 (Mathf.Sign (rigidbody2D.velocity.x) * maxSpeed, rigidbody2D.velocity.y);
+		}
+
+	}
+
+	void OnCollisionEnter2D (Collision2D collision) {
+		string tag = collision.gameObject.tag;
+		if(tag == "Player"){
+			Player.instance.ApplyDamage ();
+			Destroy (gameObject);
+		}
+	}
+}
---
+++ Assets/Scripts/Boss/Boss.cs
@@ -0,0 +1,87 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Boss : Enemy {
+	public GameObject barrelPrefab;
+	public float maxAtackInterval;
+	private Animator mAnimator;
+	private GameObject mKickObject;
+	private Transform mThrowPosition;
+	private Transform mPlayerTransform;
+	private float mMoveSpeed = 0.5f;
+	private float mAtackInterval;
+
+	void Start () {
+		mAnimator = GetComponent<Animator> ();
+		mKickObject = transform.Find ("Kick").gameObject;
+		mThrowPosition = transform.Find ("Throw");
+		mKickObject.SetActive (false);
+		mAnimator.SetFloat ("Speed", moveForce);
+		mPlayerTransform = Player.instance.transform;
+		mAtackInterval = 2.0f;
+	}
+
+	void FixedUpdate () {
+		if (life <= 0) {
+			CheckFlip (mMoveSpeed);
+			Move (-mMoveSpeed * 10);
+			mAnimator.SetTrigger ("Death");
+			enabled = false;
+			ScoreKeeper.instance.AddScore (score);
+			Destroy (gameObject, 0.8f);
+		}
+
+		mAtackInterval -= Time.deltaTime;
+
+		if (mAtackInterval < 0) {
+			Atack ();
+		}
+			
+		AnimatorStateInfo info = mAnimator.GetCurrentAnimatorStateInfo (0);
+		if (info.nameHash == Animator.StringToHash ("Base Layer.Kick")) {
+			mKickObject.SetActive (true);
+		} else {
+			mKickObject.SetActive (false);
+		}			
+
+		if (mPlayerTransform.position.x > transform.position.x) {
+			mMoveSpeed = 0.5f;
+		} else {
+			mMoveSpeed = -0.5f;
+		}
+
+		CheckFlip (mMoveSpeed);
+			
+		float distance = Vector3.Distance (mPlayerTransform.position,transform.position);
+		if(distance <= atackDistance){
+			mAnimator.SetFloat ("Speed", 0);
+		}else {
+			mAnimator.SetFloat ("Speed", moveForce);
+		}
+
+		if(info.nameHash == Animator.StringToHash("Base Layer.Walk")){
+			Move (mMoveSpeed);
+		}
+	}
+
+	private void Atack(){
+		float distance = Vector3.Distance (mPlayerTransform.position,transform.position);
+		if(distance >= 3){
+			mAnimator.SetTrigger ("Throw");
+			Invoke ("ThrowBarrel",0.3f);
+		}else {
+			mAnimator.SetTrigger ("Kick");
+		}
+
+		mAtackInterval = 120.0f;
+	}
+
+	private void ThrowBarrel(){
+
+		GameObject barrelObject =  Instantiate (barrelPrefab) as GameObject;
+		barrelObject.transform.parent = mThrowPosition;
+
+		barrelObject.transform.localPosition= new Vector3 (0,0,0);
+		barrelObject.transform.localScale = new Vector3 (1f,1f,1f);
+	}
+}
