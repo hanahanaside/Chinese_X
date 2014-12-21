42528a7af58407da0f42206f96b231d78a252585
hanahanaside <flower@flower-no-MacBook-Air.local>
2014-12-21 18:55:04 +0900

    ムチ男を作成完了

---
+++ Assets/Scripts/WhipMan/WhipMan.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WhipMan : Enemy {
+
+	private Animator mAnimator;
+	private GameObject mAtackObject;
+	private Transform mPlayerTransform;
+	private float mMoveSpeed = 0.5f;
+
+	void Start () {
+		mAnimator = GetComponent<Animator> ();
+		mAtackObject = transform.Find ("Atack").gameObject;
+		mAtackObject.SetActive (false);
+		mAnimator.SetFloat ("Speed", moveForce);
+		mPlayerTransform = Player.instance.transform;
+	}
+
+	void FixedUpdate () {
+		if(life <=0){
+			CheckFlip (mMoveSpeed);
+			Move (-mMoveSpeed * 10);
+			mAnimator.SetTrigger ("Death");
+			enabled = false;
+			ScoreKeeper.instance.AddScore (score);
+			Destroy (gameObject,0.8f);
+		}
+		AnimatorStateInfo info = mAnimator.GetCurrentAnimatorStateInfo (0);
+		if(info.nameHash == Animator.StringToHash ("Base Layer.Atack")){
+			mAtackObject.SetActive (true);
+		}else {
+			mAtackObject.SetActive (false);
+		}			
+
+		if (mPlayerTransform.position.x > transform.position.x) {
+			mMoveSpeed = 0.5f;
+		} else {
+			mMoveSpeed = -0.5f;
+		}
+
+		CheckFlip (mMoveSpeed);
+
+		float distance = Vector3.Distance (mPlayerTransform.position,transform.position);
+		if(distance <= atackDistance){
+			mAnimator.SetFloat ("Speed", 0);
+		}else {
+			mAnimator.SetFloat ("Speed", moveForce);
+		}
+
+		if(info.nameHash == Animator.StringToHash("Base Layer.Walk")){
+			Move (mMoveSpeed);
+		}
+
+	}
+
+}
