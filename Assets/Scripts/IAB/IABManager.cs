975121b23bc91f2c04ad82df1c978b3a2ba21718
hanahanaside <flower@flower-no-MacBook-Air.local>
2015-01-22 22:45:37 +0900

    アイコンを追加

--- Assets/Scripts/IAB/IABManager.cs
+++ Assets/Scripts/IAB/IABManager.cs
@@ -1,9 +1,96 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
-public class IABManager : MonoSingleton<IABManager> {
+public class IABManager : MonoSingleton<IABManager> { 
 
-	public override void OnInitialize(){
+	#if UNITY_ANDROID
+	public enum Products {
+		Ticket_1,
+		Ticket_2}
+	;
 
+	private const string PUBLIC_KEY = "";
+
+	public override void OnInitialize () {
+		GoogleIAB.init (PUBLIC_KEY);
 	}
+
+	public void PurchaseProduct (Products products) {
+		GoogleIAB.purchaseProduct ("android.test.purchased");
+	}
+
+	void OnEnable () {
+		// Listen to all events for illustration purposes
+		GoogleIABManager.billingSupportedEvent += billingSupportedEvent;
+		GoogleIABManager.billingNotSupportedEvent += billingNotSupportedEvent;
+		GoogleIABManager.queryInventorySucceededEvent += queryInventorySucceededEvent;
+		GoogleIABManager.queryInventoryFailedEvent += queryInventoryFailedEvent;
+		GoogleIABManager.purchaseCompleteAwaitingVerificationEvent += purchaseCompleteAwaitingVerificationEvent;
+		GoogleIABManager.purchaseSucceededEvent += purchaseSucceededEvent;
+		GoogleIABManager.purchaseFailedEvent += purchaseFailedEvent;
+		GoogleIABManager.consumePurchaseSucceededEvent += consumePurchaseSucceededEvent;
+		GoogleIABManager.consumePurchaseFailedEvent += consumePurchaseFailedEvent;
+	}
+
+	void OnDisable () {
+		// Remove all event handlers
+		GoogleIABManager.billingSupportedEvent -= billingSupportedEvent;
+		GoogleIABManager.billingNotSupportedEvent -= billingNotSupportedEvent;
+		GoogleIABManager.queryInventorySucceededEvent -= queryInventorySucceededEvent;
+		GoogleIABManager.queryInventoryFailedEvent -= queryInventoryFailedEvent;
+		GoogleIABManager.purchaseCompleteAwaitingVerificationEvent += purchaseCompleteAwaitingVerificationEvent;
+		GoogleIABManager.purchaseSucceededEvent -= purchaseSucceededEvent;
+		GoogleIABManager.purchaseFailedEvent -= purchaseFailedEvent;
+		GoogleIABManager.consumePurchaseSucceededEvent -= consumePurchaseSucceededEvent;
+		GoogleIABManager.consumePurchaseFailedEvent -= consumePurchaseFailedEvent;
+	}
+
+	void billingSupportedEvent () {
+		Debug.Log ("billingSupportedEvent");
+		var skus = new string[] {
+			"com.prime31.testproduct",
+			"android.test.purchased",
+			"com.prime31.managedproduct",
+			"com.prime31.testsubscription"
+		};
+		GoogleIAB.queryInventory (skus);
+	}
+
+	void billingNotSupportedEvent (string error) {
+		Debug.Log ("billingNotSupportedEvent: " + error);
+	}
+
+	void queryInventorySucceededEvent (List<GooglePurchase> purchases, List<GoogleSkuInfo> skus) {
+		Debug.Log (string.Format ("queryInventorySucceededEvent. total purchases: {0}, total skus: {1}", purchases.Count, skus.Count));
+		Prime31.Utils.logObject (purchases);
+		Prime31.Utils.logObject (skus);
+	}
+
+	void queryInventoryFailedEvent (string error) {
+		Debug.Log ("queryInventoryFailedEvent: " + error);
+	}
+
+	void purchaseCompleteAwaitingVerificationEvent (string purchaseData, string signature) {
+		Debug.Log ("purchaseCompleteAwaitingVerificationEvent. purchaseData: " + purchaseData + ", signature: " + signature);
+	}
+
+	void purchaseSucceededEvent (GooglePurchase purchase) {
+		Debug.Log ("purchaseSucceededEvent: " + purchase);
+		GoogleIAB.consumeProduct ("android.test.purchased");
+	}
+
+	void purchaseFailedEvent (string error, int response) {
+		Debug.Log ("purchaseFailedEvent: " + error + ", response: " + response);
+	}
+
+	void consumePurchaseSucceededEvent (GooglePurchase purchase) {
+		Debug.Log ("consumePurchaseSucceededEvent: " + purchase);
+	}
+
+	void consumePurchaseFailedEvent (string error) {
+		Debug.Log ("consumePurchaseFailedEvent: " + error);
+	}
+
+	#endif
 }
